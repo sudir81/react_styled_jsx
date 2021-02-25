9bb2582bb79c5275f085d555fac480c5bf0fe446
Sudheer Papineni <55796541+sudir81@users.noreply.github.com>
2021-02-25 11:33:34 -0500

    added flushtohtml

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import fs from 'fs'
 import React from 'react'
 import express from 'express'
 import ReactDOMServer from 'react-dom/server'
+import { flushToHTML } from 'styled-jsx/server'
 
 import App from '../src/App'
 
@@ -12,18 +13,38 @@ const app = express();
 
 app.get('/', (req, res) => {
     const app = ReactDOMServer.renderToString(<App />);
+    const styles = flushToHTML();
+    
+    // const indexFile = path.resolve('./build/index.html');
+    // fs.readFile(indexFile, 'utf8', (err, data) => {
+    //     if(err) {
+    //         console.error('Something went wrong: ', err);
+    //         return res.status(500).send('Better luck next time!');
+    //     }
 
-    const indexFile = path.resolve('./build/index.html');
-    fs.readFile(indexFile, 'utf8', (err, data) => {
-        if(err) {
-            console.error('Something went wrong: ', err);
-            return res.status(500).send('Better luck next time!');
-        }
+    //     return res.send(
+    //         data.replace('<div id="root"></div>', `<div id="root">${app}</div>`)
+    //     )
+    // })
 
-        return res.send(
-            data.replace('<div id="root"></div>', `<div id="root">${app}</div>`)
-        )
-    })
+    const html = `<!doctype html>
+        <html lang="en">
+            <head>
+                <meta charset="utf-8"/>
+                <link rel="icon" href="/favicon.ico"/>
+                <meta name="viewport" content="width=device-width,initial-scale=1"/>
+                <meta name="theme-color" content="#000000"/>
+                <meta name="description" content="Web site created using create-react-app"/>
+                <link rel="manifest" href="/manifest.json"/>
+                <title>React App</title>
+                ${styles}
+            </head>
+            <body>
+                <div id="root">${app}</div>
+            </body>
+        </html>
+    `
+    res.end(html)
 });
 
 app.use(express.static('./build'));
